2a6f887da862554406c47ff3b084a5089834af3b
DAKSHCTU11 <dakshctu11@gmail.com>
2024-06-18 11:12:20 +0530

    Initial Commit

---
+++ efwesfesdf/efwesfesdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  efwesfesdfApp.swift
+//  efwesfesdf
+//
+//  Created by Daksh Chaudhary on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct efwesfesdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
